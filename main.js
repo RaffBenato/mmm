5c49de0a837d953c5cfd52bbc5343ef7e4f80816
Rafael Benato <benato@email.com>
2023-04-27 11:36:27 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const logo = document.querySelector(`.logo-box`);
+const landing = document.querySelector(`.landing-main`);
+const logoInitialSize = logo.offsetWidth;
+const logoSmallSize = logoInitialSize / 4;
+const logoInitialPosition = logo.getBoundingClientRect().top;
+console.log(logoInitialPosition);
+
+let logoSize = 400;
+
+window.addEventListener("scroll", function (e) {
+  let logoPosition = logo.getBoundingClientRect().top;
+  let landingPosition = landing.getBoundingClientRect().bottom;
+  let screenPosition = window.innerHeight;
+
+  if (logoPosition < 10) {
+    logo.style.position = `fixed`;
+    logo.style.top = `10px`;
+    logo.style.width = `${logoSmallSize}px`;
+    logo.style.height = `${logoSmallSize}px`;
+    logo.style.backgroundColor = `rgba(245, 245, 245, 1)`;
+  }
+
+  if (landingPosition > screenPosition - 50) {
+    logo.style.width = `${logoInitialSize}px`;
+    logo.style.height = `${logoInitialSize}px`;
+    logo.style.position = `absolute`;
+    logo.style.top = `${logoInitialPosition}px`;
+    logo.style.backgroundColor = `rgba(245, 245, 245, 0.24)`;
+  }
+});
