220524516c118531b1dc74152b094ea8566c956c
Rafael Benato <benato@email.com>
2023-05-02 11:44:08 +0100

    commit

---
+++ index2/main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  const slogan1 = document.querySelector(".slogan1");
+  const slogan2 = document.querySelector(".slogan2");
+  const slogan3 = document.querySelector(".slogan3");
+  slogan1.style.animationDelay = "0s";
+  slogan2.style.animationDelay = "0s";
+  slogan3.style.animationDelay = "0s";
+};
