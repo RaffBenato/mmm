3f84b9c6263a24c422782459a5aa982cc7a7fb3f
Rafael Benato <benato@email.com>
2023-06-23 16:23:03 +0100

    commit

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
-"use strict";
+window.onload = function () {
+  const slogan1 = document.querySelector(".slogan1");
+  const slogan2 = document.querySelector(".slogan2");
+  const slogan3 = document.querySelector(".slogan3");
+  slogan1.style.animationDelay = "1.5s";
+  slogan2.style.animationDelay = "1.5s";
+  slogan3.style.animationDelay = "1.5s";
+};
 
-const logo = document.querySelector(`.logo-box`);
-const landing = document.querySelector(`.landing-main`);
-const logoInitialSize = logo.offsetWidth;
-const logoSmallSize = logoInitialSize / 3;
-const logoInitialPosition = logo.getBoundingClientRect().top;
-console.log(logoInitialPosition);
+const menuBtn = document.querySelector(".menu-btn");
+const menuLine1 = document.querySelector(".btn-line1");
+const menuLine2 = document.querySelector(".btn-line2");
+const menuLine3 = document.querySelector(".btn-line3");
+const menu = document.querySelector(".menu");
 
-let logoSize = 400;
+//Set initial State of Menu
 
-window.addEventListener("scroll", function (e) {
-  let logoPosition = logo.getBoundingClientRect().top;
-  let landingPosition = landing.getBoundingClientRect().bottom;
-  let screenPosition = window.innerHeight;
+let showMenu = false;
 
-  if (logoPosition < 10) {
-    logo.style.position = `fixed`;
-    logo.style.top = `10px`;
-    logo.style.width = `${logoSmallSize}px`;
-    logo.style.height = `${logoSmallSize}px`;
-    logo.style.backgroundColor = `rgba(245, 245, 245, 1)`;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menuLine1.classList.add("close");
+    menuLine2.classList.add("close");
+    menuLine3.classList.add("close");
+    menu.classList.add("show");
+
+    //Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menuLine1.classList.remove("close");
+    menuLine2.classList.remove("close");
+    menuLine3.classList.remove("close");
+    menu.classList.remove("show");
+
+    //Set Menu State
+    showMenu = false;
   }
-
-  if (landingPosition > screenPosition - 50) {
-    logo.style.width = `${logoInitialSize}px`;
-    logo.style.height = `${logoInitialSize}px`;
-    logo.style.position = `absolute`;
-    logo.style.top = `${logoInitialPosition}px`;
-    logo.style.backgroundColor = `rgba(245, 245, 245, 0.24)`;
-  }
-});
+}
